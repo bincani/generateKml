fd27ae83b2a680b0e628fcde3d7a5fdb384be756
Ben Incani <ben.incani@gmail.com>
2019-09-23 21:32:43 +1000

    fixes

--- googleGetLatLon.php
+++ googleGetLatLon.php
@@ -19,7 +19,9 @@ class GenerateKML {
 
     private static $_debug = 1;
     private static $_test = 0;
+
     private $apiKey;
+    private $region = "AU";
 
     /**
      * run
@@ -29,19 +31,37 @@ class GenerateKML {
         $this->apiKey = file_get_contents('_apiKey.txt');
         $this->log(sprintf("this->apiKey: %s", $this->apiKey));
 
-        $inputFile = sprintf("%s/data/2017-2018.csv", getcwd());
-        $addresses = $this->readInputData($inputFile, $limit = 2);
+        $addresses = array();
+
+        if (self::$_test) {
+            //$this->region = "USA";
+            //$addresses[] = "1600 Pennsylvania Ave NW Washington DC 20500";
+            $this->region = "AU";
+            $addresses[] = "Shop 3036, Level 3, Westfield Bondi Junction, 500 Oxford St";
+        }
+        else {
+            $inputFile = sprintf("%s/data/2018-2019.csv", getcwd());
+            $addresses = $this->readInputData($inputFile, $limit = 0);
+        }
 
         $this->log(sprintf("addresses: %s", count($addresses)));
 
 
         if (self::$_test) {
+            // lat, lon
             $coords = array();
-            $coords[] = "-33.89721, 151.24733";
-            $coords[] = "-33.89812, 151.25018";
+            $coords[] = [
+                'address' => 'test address 1',
+                'coords' => [151.24733,-33.89721]
+            ];
+            $coords[] = [
+                'address' => 'test address 2',
+                'coords' => [151.25018,-33.89812]
+            ];
         }
         else {
-            $coords = generateAllCoords($addresses);
+            $coords = $this->generateAllCoords($addresses);
+            //$this->log(sprintf("coords: %s", print_r($coords, true)));
         }
 
         $this->createKML($coords);
@@ -55,18 +75,20 @@ class GenerateKML {
         $placemarks = "";
         $cnt = 0;
         foreach($coords as $coord) {
-            $name = sprintf("%d", $cnt++);
+            $name = sprintf("%d - %s", $cnt++, $coord['address']);
+            //$this->log(sprintf("coord: %s", print_r($coord, true)));
             $placemark = sprintf(
-                "%s<Placemark><name>%s</name><description></description><styleUrl>#sale</styleUrl><Point><coordinates>%s</coordinates></Point></Placemark>",
+                "%s<Placemark><name>%s</name><description></description><styleUrl>#sale</styleUrl><Point><coordinates>%f,%f</coordinates></Point></Placemark>",
                 str_repeat(" ", 6),
                 $name,
-                $coord
+                $coord['coords'][0],
+                $coord['coords'][1]
             );
             $placemarks .= $placemark . "\n";
         }
         $placemarks = substr($placemarks, 0, -1); // remove last char
         $template = preg_replace("/%PLACEMARKS%/", $placemarks, $template);
-        $filename = sprintf("%s/%s_placemarks.kml", dirname(__FILE__), date('Ymd-his'));
+        $filename = sprintf("%s/output/%s_placemarks.kml", dirname(__FILE__), date('Ymd-his'));
         file_put_contents($filename, $template);
     }
 
@@ -77,18 +99,25 @@ class GenerateKML {
     public function readInputData($inputFile, $limit = 0) {
         $addresses = array();
         $fh = fopen($inputFile,"r");
+        $header;
         $cnt = 0;
         while(!feof($fh)) {
+            $record = fgetcsv($fh);
+            // skip header
+            if ($cnt == 0) {
+                $header = $record;
+                $cnt++;
+                continue;
+            }
             if ($limit != 0 && $cnt++ > $limit) {
-                //echo sprintf("limit %d reached!\n", $limit);
+                //$this->log(sprintf("limit %d reached!", $limit));
                 break;
             }
-            $record = fgetcsv($fh);
             //$this->log(sprintf("record: %s", print_r($record, true)) );
             $street = $record[2];
             $street = preg_replace("/\\\\n/", ", ", $record[2]);
             //$this->log(sprintf("street: %s", $street));
-            $address = sprintf("%s, %s, %s, %s", $street, $record[3], $record[4], $record[5]);
+            $address = sprintf("%s,%s,%s,%s", $street, $record[3], $record[4], $record[5]);
             //$this->log(sprintf("address: %s", $address));
             $addresses[] = $address;
         }
@@ -102,7 +131,17 @@ class GenerateKML {
     public function generateAllCoords($addresses) {
         $coords = array();
         foreach($addresses as $address) {
-            $coords = $this->generateCoords($address);
+            try {
+                $coord = $this->generateCoords($address);
+                $coords[] = array(
+                    'address' => $address,
+                    'coords' => $coord
+                );
+            }
+            catch(Exception $ex) {
+                $this->log(sprintf("Error: %s", $ex->getMessage()));
+                $this->log(sprintf("%s", $address));
+            }
         }
         return $coords;
     }
@@ -111,30 +150,33 @@ class GenerateKML {
      * generateCoords
      */
     public function generateCoords($address) {
-        //$region = "USA";
-        //$address = "1600 Pennsylvania Ave NW Washington DC 20500";
-
-        $region = "AU";
-        $address = "Shop 3036, Level 3, Westfield Bondi Junction, 500 Oxford St";
         $address = str_replace(" ", "+", $address);
-
-        $auth = sprintf("&key=%s", self::API_KEY);
+        $auth = sprintf("&key=%s", $this->apiKey);
         $requestUrl = sprintf(
             "https://maps.google.com/maps/api/geocode/json?address=%s&sensor=false&region=%s%s",
             $address,
-            $region,
+            $this->region,
             $auth
         );
-        echo sprintf("get: %s\n", $requestUrl);
+        //$this->log(sprintf("get: %s", $requestUrl));
         $json = file_get_contents($requestUrl);
-        $json = json_decode($json);
-
-        //echo sprintf("json: %s\n", print_r($json, true));
-
-        $lng = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'};
-        $lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
-        //echo sprintf("%f,%f\n", $lng, $lat);
-
+        $geocode = json_decode($json);
+        if ($geocode === null) {
+            throw new Exception(sprintf("cannot geocode address - %s", $json));
+        }
+        else if (
+            !isset($geocode->{'results'}[0]->{'geometry'}->{'location'}->{'lng'})
+            ||
+            !isset($geocode->{'results'}[0]->{'geometry'}->{'location'}->{'lat'})
+        ) {
+            throw new Exception(sprintf("cannot geocode address - %s", print_r($geocode, true)) );
+        }
+        else {
+            //$this->log(sprintf("json: %s", print_r($geocode, true)));
+            $lng = $geocode->{'results'}[0]->{'geometry'}->{'location'}->{'lng'};
+            $lat = $geocode->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
+            //$this->log(sprintf("%s: %f,%f", __METHOD__, $lng, $lat));
+        }
         return [$lng, $lat];
     }
 
